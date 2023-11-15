fbb9b89e444b2083b4219d634deb2f08915893bd
BeanFlame (豆焰) <beanflame@qq.com>
2023-11-15 15:03:58 +0800

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -74,8 +74,8 @@
                     <div class="title">{{ $t('home.code.title') }}</div>
                     <!-- 木有办法，prism会按照格式渲染 -->
 <pre><code class="language-css" id="code">    "glass.background-color" = "{{ state.color }}";<template v-if="state.blur || state.saturation < 100">
-    "glass.backdrop-filter" = "<template
-        v-if="state.blur"> blur({{state.blur}}px)</template><template
+    "glass.backdrop-filter" = <template
+        v-if="state.blur"> "blur({{state.blur}}px)</template><template
         v-if="state.saturation < 100"> saturate({{ state.saturation / 100 }})</template>";</template><template
         v-if="state.radius">
     "glass.border-radius" = "{{ state.radius }}px";</template>
